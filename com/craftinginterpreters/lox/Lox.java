cc2e3ffdee9fb86f5550c5802947eaad6bff2109
Marcus Guimaraes <mzg218@nyu.edu>
2021-11-24 18:11:48 -0500

    save

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -9,7 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    static boolean hadError = false;
     public static void main(String[] args) throws IOException {
+
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
             System.exit(64); // exit codes from UNIX sysexits.h
@@ -18,5 +20,43 @@ public class Lox {
         } else {
             runPrompt();
         }
+    }
+
+    // runs a Lox file from disk to halting.
+    public static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+    }
+
+    // initializes a Lox REPL.
+    public static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null) break;
+            run(line);
+        }
+    }
+
+    // runs a string of Lox code
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    static void error(int lineNumber, String message) {
+        report(lineNumber, "", message);
+    }
+
+    private static void report(int lineNumber, String where, String message) {
+        System.err.println("[line " + lineNumber + "] Error" + where + ": " + message);
+        hadError = true;
     }
 }
