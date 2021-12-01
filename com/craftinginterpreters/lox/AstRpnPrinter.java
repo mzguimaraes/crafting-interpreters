02a3d558ccf33fdea45839fa75241445492e95e4
Marcus Guimaraes <mzg218@nyu.edu>
2021-11-30 20:47:56 -0500

    reverse Polish notation

---
+++ com/craftinginterpreters/lox/AstRpnPrinter.java
@@ -0,0 +1,55 @@
+package com.craftinginterpreters.lox;
+
+public class AstRpnPrinter implements Expr.Visitor<String> {
+
+    public String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return expr.left.accept(this) + " " + 
+               expr.right.accept(this) + " " + 
+               expr.operator.lexeme;
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return expr.expression.accept(this);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null) return "nil";
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return expr.right.accept(this) + " " + expr.operator.lexeme;
+    }
+
+    public static void main(String[] args) {
+        Expr expression = 
+        new Expr.Grouping(
+            new Expr.Binary(
+                new Expr.Binary(
+                    new Expr.Literal(1), 
+                    new Token(TokenType.PLUS, "+", null, 1), 
+                    new Expr.Literal(2)
+                ),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Binary(
+                    new Expr.Literal(4), 
+                    new Token(TokenType.PLUS, "+", null, 1), 
+                    new Expr.Unary(
+                        new Token(TokenType.MINUS, "-", null, 1), 
+                        new Expr.Literal(3)
+                    )
+                )
+            )
+        );
+
+        System.out.println(new AstRpnPrinter().print(expression));
+    }
+}
