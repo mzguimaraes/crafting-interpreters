6fb354a50498c01d657c5d2c7e943ba6e0654e79
Marcus Guimaraes <mzg218@nyu.edu>
2022-01-17 16:31:00 -0500

    Create Makefile

--- clox/main.c
+++ clox/main.c
@@ -1,7 +1,10 @@
+#include <stdio.h> 
+
 #include "common.h"
 #include "chunk.h"
 
 int main(int argc, const char* argv[]) {
+    printf("Testing chunk code\n");
     Chunk chunk;
     initChunk(&chunk);
     writeChunk(&chunk, OP_RETURN);
