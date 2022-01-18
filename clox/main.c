ec82f510dd8c5f48c6d647bfb85af54fc95105e5
Marcus Guimaraes <mzg218@nyu.edu>
2022-01-18 15:00:47 -0500

    chapter 14

--- clox/main.c
+++ clox/main.c
@@ -1,12 +1,17 @@
-#include <stdio.h> 
-
 #include "common.h"
 #include "chunk.h"
+#include "debug.h"
 
 int main(int argc, const char* argv[]) {
-    printf("Testing chunk code\n");
     Chunk chunk;
     initChunk(&chunk);
-    writeChunk(&chunk, OP_RETURN);
+
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_RETURN, 123);
+
+    disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
 }
